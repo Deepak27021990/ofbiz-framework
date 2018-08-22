b6b1a9c1e7009b4253c8d3bd1e1a0eac44885624
Deepak Nigam <deepak.nigam@hotwaxsystems.com>
2018-08-22 09:57:00 +0530

    Added feature to provide reserver after date for order items. (OFBiz-10534)

--- applications/order/src/main/java/org/apache/ofbiz/order/order/OrderServices.java
+++ applications/order/src/main/java/org/apache/ofbiz/order/order/OrderServices.java
@@ -1198,6 +1198,13 @@ public class OrderServices {
                         continue;
                     }
                     GenericValue orderItem = itemValuesBySeqId.get(orderItemShipGroupAssoc.get("orderItemSeqId"));
+                    if ("SALES_ORDER".equals(orderTypeId) && orderItem != null) {
+                        //If the 'reserveAfterDate' is not yet come don't reserve the inventory
+                        Timestamp reserveAfterDate = orderItem.getTimestamp("reserveAfterDate");
+                        if (UtilValidate.isNotEmpty(reserveAfterDate) && reserveAfterDate.after(UtilDateTime.nowTimestamp())) {
+                            continue;
+                        }
+                    }
                     GenericValue orderItemShipGroup = orderItemShipGroupAssoc.getRelatedOne("OrderItemShipGroup", false);
                     String shipGroupFacilityId = orderItemShipGroup.getString("facilityId");
                     String itemStatus = orderItem.getString("statusId");
